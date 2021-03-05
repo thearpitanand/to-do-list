1c970f29a0c273e9cd1e2bc0b1894d8a749a48ea
Arpit Anand <arpitssanand@gmail.com>
2021-03-05 14:40:09 +0530

    Add Procfile and package.json, Port Update

--- app.js
+++ app.js
@@ -148,6 +148,11 @@ app.post("/delete", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => {
   console.log("Server is listening at Port 3000");
 });
